ae616f24fc03bae927dc4a679f51bf7da88cfc8c
Vishnu <nandam.vishnutejaa@gmail.com>
2024-03-01 08:17:54 -0700

    Initial Commit

---
+++ ExpenseTracker/ExpenseTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExpenseTrackerApp.swift
+//  ExpenseTracker
+//
+//  Created by Vishnu Tejaa on 3/1/24.
+//
+
+import SwiftUI
+
+@main
+struct ExpenseTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
